19e9ad921b7ef21b76cc6dce1ff3a56184202060
MohamedAgiba <mohamed.agiba@cic.ae>
2026-07-29 21:26:27 +0300

    004 Event Binding Syntax

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('pw');
+onButtonClick(){
+  console.log('click');
+}
 }
