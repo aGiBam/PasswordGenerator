d833b607aa47fb9765ad1f7f74cf0a6cffcc462e
MohamedAgiba <mohamed.agiba@cic.ae>
2026-08-05 01:39:57 +0300

    005

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,7 +6,30 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
+  password = "";
+  includeNumbers = signal(false);
+  includeSymbols = signal(false);
+  includeLetters = signal(false);
+
+  onChangeUseNumbers(){
+      console.log("changedNumbers");
+
+    this.includeNumbers.set(!this.includeNumbers());
+  }
+  onChangeUseSymbols(){
+      console.log("changedSymbols");
+
+    this.includeSymbols.set(!this.includeSymbols());
+  }
+  onChangeUseLetters(){
+      console.log("changedLetters");
+
+    this.includeLetters.set(!this.includeLetters());
+  }
 onButtonClick(){
-  console.log('click');
+  console.log(this.includeNumbers(),'numbers');
+  console.log(this.includeSymbols(),'symbols');
+  console.log(this.includeLetters(),'letters');
+  this.password = "My Assword !!!";
 }
 }
